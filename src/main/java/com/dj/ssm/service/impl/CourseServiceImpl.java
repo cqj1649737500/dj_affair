4d0c4692e40d25d9b7cf31bf31b81bad7e025212
cuiqj <cqj15235193790@163.com>
2020-07-22 23:14:04 +0800

    CourseService添加

---
+++ src/main/java/com/dj/ssm/service/impl/CourseServiceImpl.java
@@ -0,0 +1,10 @@
+package com.dj.ssm.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.dj.ssm.mapper.CourseMapper;
+import com.dj.ssm.pojo.Course;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CourseServiceImpl extends ServiceImpl<CourseMapper, Course> {
+}
