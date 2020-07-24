dbb11267369b186bba25294a1edfe02690c3fc5d
yc <yc15034174737@163.com>
2020-07-24 09:20:38 +0800

    学生选择课程jsp

---
+++ src/main/java/com/dj/ssm/service/StuCourseService.java
@@ -0,0 +1,7 @@
+package com.dj.ssm.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.dj.ssm.pojo.StuCourse;
+
+public interface StuCourseService extends IService<StuCourse> {
+}
