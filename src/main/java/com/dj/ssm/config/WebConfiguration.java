012b5574a1a602ea00a15e5dfb6a6b200191429b
cuiqj <cqj15235193790@163.com>
2020-07-27 09:13:59 +0800

    拦截器应用

--- src/main/java/com/dj/ssm/config/WebConfiguration.java
+++ src/main/java/com/dj/ssm/config/WebConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistration
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration
+@Configuration
 public class WebConfiguration implements WebMvcConfigurer {
 
     @Autowired
